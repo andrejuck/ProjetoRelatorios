447b4900e8132cac5782a50bf04e6989efe82b7b
andrejuck <andrejuck@hotmail.com>
2018-12-10 18:41:56 -0200

    Adicionar arquivos de projeto.

---
+++ ProjetoTesteRelatorios/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProjetoTesteRelatorios.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
