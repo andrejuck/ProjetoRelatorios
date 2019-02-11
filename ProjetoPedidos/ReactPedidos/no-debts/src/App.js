1b8b0e8080b6bea4065c9fbb2bc857c8717bd2bf
andrejuck <andrejuck@hotmail.com>
2019-02-11 20:29:55 -0200

    semantic ui

--- ProjetoPedidos/ReactPedidos/no-debts/src/App.js
+++ ProjetoPedidos/ReactPedidos/no-debts/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import './dist/semantic.css';
 import './App.css';
 
 class App extends Component {
@@ -7,9 +7,8 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            <button className="ui button primary inverted">Teste</button>
           </p>
           <a
             className="App-link"
