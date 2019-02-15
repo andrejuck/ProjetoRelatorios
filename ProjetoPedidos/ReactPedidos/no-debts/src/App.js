0539377ea6db3b959f1492951148dbcd659c0348
andrejuck <andrejuck@hotmail.com>
2019-02-15 21:46:16 -0200

    fucking header

--- ProjetoPedidos/ReactPedidos/no-debts/src/App.js
+++ ProjetoPedidos/ReactPedidos/no-debts/src/App.js
@@ -3,8 +3,21 @@ import React, { Component } from 'react';
 class App extends Component {
     render() {
         return (
-            <div>
+            <div className="ui">
+                <div className="navbar">
+                    <div className="navbar container">
+                        <div className="navbar menu">
+                            <div className="itens">
+                                <img className="image" alt="" src={require("./resources/logo.png")}></img>
+                                <a href="" >Home</a>
+                            </div>
 
+                        </div>
+                    </div>
+                </div>
+                <div className="ui container">
+
+                </div>
             </div>
         );
     }
